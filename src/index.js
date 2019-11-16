2a9c020714afbe23b00aef35cb2275e5bf89fdf6
EdLuigi <luigi.ciuffi@hotmail.com>
2019-11-16 03:53:48 -0300

    api finalizada

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
 mongoose.connect("mongodb+srv://semana:semana@rocketseat-instagram-clone-k8biw.mongodb.net/test?retryWrites=true&w=majority", {
-     useNewUrlParser: true,
-     useUnifiedTopology: true
+	useNewUrlParser: true,
+	useUnifiedTopology: true
 });
 
-app.use(require('./routes'));
+app.use(cors());
 
-app.listen(3333);
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads", "resized")));
+
+app.use(require("./routes"));
+
+app.listen(8000);
